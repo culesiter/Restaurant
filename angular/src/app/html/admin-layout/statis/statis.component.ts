38b2f00707ef813ef93b1c5d23a3e3747d971af9
t.c.manh <t.c.manh1997@gmail.com>
2019-08-02 23:57:26 +0700

    1

--- angular/src/app/html/admin-layout/statis/statis.component.ts
+++ angular/src/app/html/admin-layout/statis/statis.component.ts
@@ -9,6 +9,7 @@ const moment = require('moment');
 })
 export class StatisComponent implements OnInit {
   @Input() chartData: any[] = [{ data: [] }];
+  @Input() chartData2: any[] = [{ data: [] }];
   private data1 = [{
     label: '1st Year',
     data: [21, 56, 4, 31, 45, 15, 57, 61, 9, 17, 24, 59]
@@ -19,10 +20,14 @@ export class StatisComponent implements OnInit {
   }];
   private hddm;
   private title: any;
+   private title2: any;
   private chartOptions;
+  private chartOptions2;
   private mchart = [];
   private labels;
   private colors;
+  private labels2;
+  private colors2;
   constructor(private hoadonS: HoadonService) { }
 
   ngOnInit() {
@@ -78,12 +83,65 @@ export class StatisComponent implements OnInit {
       this.chartData = JSON.parse(sessionStorage.getItem('chart'));
     });
   }
+  laydsHoadon2() {
+    this.hoadonS.laydanhsach().subscribe(res => {
+      this.hddm = [];
+      res.forEach(element => {
+        if (element.tinhtrang === 2) {
+          this.hddm.push(element);
+          const m = element.thoidiemtao;
+          this.laycthd2(element._id, moment(element.thoidiemden, 'DD/MM/YYYY').month());
+        }
+      });
+    });
+  }
+  laycthd2(id, month) {
+    this.hoadonS.getCTHD(id).subscribe(res => {
+      res.forEach(element => {
+        if (element._idmonan) {
+          const value = {
+            label: element._idmonan.ten,
+            id: element._idmonan._id,
+            data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+          };
+          // tslint:disable-next-line:max-line-length
+          value.data[month] = element.soluongmonan * element._idmonan.gia - (element.soluongmonan * element._idmonan.gia * element._idmonan.khuyenmai / 100);
+          console.log(value.data[month]);
+          
+          if (sessionStorage.getItem('chart') === null || JSON.parse(sessionStorage.getItem('chart')).length === 0) {
+            this.mchart.push(value);
+            sessionStorage.setItem('chart', JSON.stringify(this.mchart));
+          } else {
+            this.mchart = JSON.parse(sessionStorage.getItem('chart'));
+            let index = -1;
+            for (let i = 0; i < this.mchart.length; i++) {
+              if (this.mchart[i].id === element._idmonan._id) {
+                index = i;
+                break;
+              }
+            }
+            if (index === -1) {
+              this.mchart.push(value);
+              sessionStorage.setItem('chart', JSON.stringify(this.mchart));
+            } else {
+              const temp = this.mchart[index];
+              // tslint:disable-next-line:max-line-length
+              temp.data[month] = temp.data[month] + element.soluongmonan * element._idmonan.gia - (element.soluongmonan * element._idmonan.gia * element._idmonan.khuyenmai / 100);
+              this.mchart[index] = temp;
+              sessionStorage.setItem('chart', JSON.stringify(this.mchart));
+            }
+          }
+        }
+      });
+      this.chartData2 = JSON.parse(sessionStorage.getItem('chart'));
+    });
+  }
   showmodal() {
     sessionStorage.setItem('chart', JSON.stringify([]));
     this.laydsHoadon();
     if (sessionStorage.getItem('chart')) {
 
-      this.title = 'Bar Chart Example in Angular 4';
+      this.title = 'Biểu Đồ tiêu thụ món ăn năm 2019';
 
       // ADD CHART OPTIONS.
       this.chartOptions = {
@@ -92,6 +150,10 @@ export class StatisComponent implements OnInit {
           yAxes: [{
             ticks: {
               stepSize: 1
+            },
+            scaleLabel: {
+              display: true,
+              labelString: 'Số lượng mua'
             }
           }]
         }
@@ -104,7 +166,36 @@ export class StatisComponent implements OnInit {
       // }, 3000);
     }
   }
+  showmodal2() {
+    sessionStorage.setItem('chart', JSON.stringify([]));
+    this.laydsHoadon2();
+    if (sessionStorage.getItem('chart')) {
 
+      this.title2 = 'Biểu Đồ tiêu thụ món ăn năm 2019';
+
+      // ADD CHART OPTIONS.
+      this.chartOptions2 = {
+        responsive: true,   // THIS WILL MAKE THE CHART RESPONSIVE (VISIBLE IN ANY DEVICE).
+        scales: {
+          yAxes: [{
+            ticks: {
+              stepSize: 50000
+            },
+            scaleLabel: {
+              display: true,
+              labelString: 'VNĐ'
+            }
+          }]
+        }
+      };
+      // tslint:disable-next-line:max-line-length
+      this.labels2 = ['Tháng 1', 'Tháng 2', 'Tháng 2', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', 'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12'];
+      // setTimeout(() => {
+      //   console.log(JSON.parse(sessionStorage.getItem('chart')));
+
+      // }, 3000);
+    }
+  }
 
 
   // CHART CLICK EVENT.
